fb29927fd1badf99914bdf346a4180e9bf60770b
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-03-21 15:34:19 +0100

    Create a new feature for user specific components
    - Install `joi` and `@hookform/resolvers` packages to validate forms
      created with `react-hook-form` package with `joi` schema.
    - Create new feature called `users` that will contain all components
      related to users and their data.
    - Create `SearchUserForm` component that will be used to search for
      users by their username.

---
+++ src/features/users/components/SearchUserForm.jsx
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import { forwardRef } from 'react';
+import Joi from 'joi';
+
+import { TextField, Button, Card, CardActions, Stack } from '@mui/material';
+import { Search } from '@mui/icons-material';
+
+import { Controller, useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+
+const schema = Joi.object({
+  username: Joi.string().trim().alphanum().min(2).max(20).required().label('username'),
+});
+
+const SearchUserForm = forwardRef(({ onSearchUser, ...rest }, ref) => {
+  const { control, formState, handleSubmit } = useForm({
+    mode: 'all',
+    defaultValues: {
+      username: '',
+    },
+    resolver: joiResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    if (formState.isValid && formState.isDirty) {
+      onSearchUser(data);
+    }
+  };
+
+  return (
+    <Card component="form" onSubmit={handleSubmit(onSubmit)} ref={ref} {...rest}>
+      <CardActions>
+        <Stack direction="row" spacing={1} sx={{ width: 1, alignItems: 'center' }}>
+          <Controller
+            name="username"
+            control={control}
+            render={({ field, fieldState }) => (
+              <TextField
+                variant="outlined"
+                placeholder="AniList Username"
+                error={Boolean(fieldState.error)}
+                fullWidth
+                {...field}
+              />
+            )}
+          />
+          <Button
+            type="submit"
+            disabled={!(formState.isValid && formState.isDirty)}
+            variant="contained"
+            size="large"
+            startIcon={<Search />}
+          >
+            Search
+          </Button>
+        </Stack>
+      </CardActions>
+    </Card>
+  );
+});
+
+SearchUserForm.displayName = 'SearchUserForm';
+
+SearchUserForm.propTypes = {
+  onSearchUser: PropTypes.func.isRequired,
+};
+
+export default SearchUserForm;
---
+++ src/features/users/index.js
@@ -0,0 +1,1 @@
+export { default as SearchUserForm } from './components/SearchUserForm';
