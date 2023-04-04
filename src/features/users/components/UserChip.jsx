a701aa80ae473f31d8459716196c2cb67b08c257
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-04-04 21:37:47 +0200

    Add slight `border-radius` to `UserChip` component
    - Add slight `border-radius` to `UserChip` component to make it look
      more eye pleasing.

--- src/features/users/components/UserChip.jsx
+++ src/features/users/components/UserChip.jsx
@@ -5,7 +5,7 @@ import { Box, Stack, Typography } from '@mui/material';
 
 const UserChip = forwardRef(({ user, ...rest }, ref) => {
   return (
-    <Box sx={{ '&:hover': { background: 'rgba(0, 0, 0, 0.2)' } }} ref={ref} {...rest}>
+    <Box sx={{ 'borderRadius': 1, '&:hover': { background: 'rgba(0, 0, 0, 0.2)' } }} ref={ref} {...rest}>
       <Stack direction="row" spacing={1} sx={{ alignItems: 'center', p: 1 }}>
         <Box component="img" src={user.avatar.large} alt={user.name} sx={{ width: 32, height: 32 }} />
         <Typography variant="button">{user.name}</Typography>
