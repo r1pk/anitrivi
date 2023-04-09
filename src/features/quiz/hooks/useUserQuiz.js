dfcef7f148b7f5570156e8538556f5f0bdca9569
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-04-09 22:05:57 +0200

    Rename variables to be more general and accurate
    - Remove `Quiz` word from variables indicating the current quiz status.
    - Rename variable containing available series to `availableSeries`.
    - Rename `user` variable to `targetUser` which is more descriptive
      that it is the user whose quiz is being displayed.

--- src/features/quiz/hooks/useUserQuiz.js
+++ src/features/quiz/hooks/useUserQuiz.js
@@ -7,40 +7,40 @@ import { evaluateAnswer } from '../utils/evaluate-answer';
 export const useUserQuiz = (userId) => {
   const { data, isFetched } = useUserProfile(userId);
 
-  const [isQuizFinished, setIsQuizFinished] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
   const [featuredAnime, setFeaturedAnime] = useState(null);
   const [guessHistory, setGuessHistory] = useState([]);
 
-  const isQuizReady = isFetched && !!featuredAnime;
-  const userAnimePool = useMemo(() => data?.lists.map((list) => list.entries).flat(), [data]);
+  const isReady = isFetched && !!featuredAnime;
+  const availableSeries = useMemo(() => data?.lists.map((list) => list.entries).flat(), [data]);
 
   useEffect(
     function pickFeaturedAnime() {
-      if (!userAnimePool || userAnimePool.length === 0) return;
+      if (!availableSeries || availableSeries.length === 0) return;
 
       const MILLISECONDS_IN_DAY = 1000 * 60 * 60 * 24;
-      const randomIndex = Math.floor(Date.now() / MILLISECONDS_IN_DAY) % userAnimePool.length;
+      const randomIndex = Math.floor(Date.now() / MILLISECONDS_IN_DAY) % availableSeries.length;
 
-      setFeaturedAnime(userAnimePool[randomIndex]);
+      setFeaturedAnime(availableSeries[randomIndex]);
     },
-    [userAnimePool]
+    [availableSeries]
   );
 
   const guessFeaturedAnime = (answer) => {
     const evaluation = evaluateAnswer(featuredAnime.media, answer.media);
 
     if (answer.mediaId === featuredAnime.mediaId) {
-      setIsQuizFinished(true);
+      setIsFinished(true);
     }
 
     setGuessHistory([{ anime: answer.media, evaluation: evaluation }, ...guessHistory]);
   };
 
   return {
-    isQuizReady: isQuizReady,
-    isQuizFinished: isQuizFinished,
-    user: data?.user,
-    userAnimePool: userAnimePool,
+    isReady: isReady,
+    isFinished: isFinished,
+    targetUser: data?.user,
+    availableSeries: availableSeries,
     featuredAnime: featuredAnime,
     guessHistory: guessHistory,
     guessFeaturedAnime: guessFeaturedAnime,
