f3cf42221f0796ca57dd8a3e41c98a7e9bba18fb
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-04-17 01:09:27 +0200

    Change appearance of `EvaluationChip` component
    - Change appearance of `EvaluationChip` component label to be thinner
      and smaller than the displayed value.

--- src/components/quiz/EvaluationChip.jsx
+++ src/components/quiz/EvaluationChip.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { forwardRef } from 'react';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { ArrowUpward, ArrowDownward } from '@mui/icons-material';
 
 import { getOrDefault } from '@/utils/get-or-default';
@@ -18,9 +18,12 @@ const EvaluationChip = forwardRef(({ label, value, evaluation, ...rest }, ref) =
 
   return (
     <Box sx={{ px: 1, py: 0.5, borderRadius: 1, backgroundColor: color }} ref={ref} {...rest}>
-      <Typography variant="button" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        {label}: {getOrDefault(value)} {isNumericEvaluation && icon[evaluation]}
-      </Typography>
+      <Stack direction="row" gap={0.5} sx={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Typography variant="caption">{label}:</Typography>
+        <Typography variant="button" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          {getOrDefault(value)} {isNumericEvaluation && icon[evaluation]}
+        </Typography>
+      </Stack>
     </Box>
   );
 });
