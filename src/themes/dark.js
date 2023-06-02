e5aae239e681b90134f84ba0d08e29ceeac1b85c
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-06-02 21:49:04 +0200

    Remove `base` styles from `themes` folder
    - Clear `MuiCardContent` style overrides from `base` theme because
      they are no longer needed.
    - Remove empty `base.js` file.

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -1,23 +1,18 @@
 import { createTheme, responsiveFontSizes } from '@mui/material';
 import { green, grey, orange, red } from '@mui/material/colors';
-import { deepmerge } from '@mui/utils';
-
-import { base } from './base';
 
 export const dark = responsiveFontSizes(
-  createTheme(
-    deepmerge(base, {
-      palette: {
-        mode: 'dark',
-        evaluation: {
-          correct: green[700],
-          higher: red[700],
-          lower: red[700],
-          partial: orange[700],
-          incorrect: red[700],
-          unknown: grey[700],
-        },
+  createTheme({
+    palette: {
+      mode: 'dark',
+      evaluation: {
+        correct: green[700],
+        higher: red[700],
+        lower: red[700],
+        partial: orange[700],
+        incorrect: red[700],
+        unknown: grey[700],
       },
-    })
-  )
+    },
+  })
 );
