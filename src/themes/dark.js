021d0802cbcb4659755e0eea72501f661f476ff1
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-06-08 00:50:46 +0200

    Add different colors for anime genres

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -1,17 +1,36 @@
-import { createTheme, responsiveFontSizes } from '@mui/material';
-import { green, grey, orange, red } from '@mui/material/colors';
+import { colors, createTheme, responsiveFontSizes } from '@mui/material';
 
 export const dark = responsiveFontSizes(
   createTheme({
     palette: {
       mode: 'dark',
       evaluation: {
-        correct: green[700],
-        higher: red[700],
-        lower: red[700],
-        partial: orange[700],
-        incorrect: red[700],
-        unknown: grey[700],
+        correct: colors.green[700],
+        higher: colors.red[700],
+        lower: colors.red[700],
+        partial: colors.orange[700],
+        incorrect: colors.red[700],
+        unknown: colors.grey[700],
+      },
+      genre: {
+        'Action': colors.lime[900],
+        'Adventure': colors.purple[900],
+        'Comedy': colors.deepOrange[900],
+        'Drama': colors.blueGrey[900],
+        'Ecchi': colors.pink[900],
+        'Fantasy': colors.lightBlue[900],
+        'Horror': colors.brown[900],
+        'Mahou Shoujo': colors.green[900],
+        'Mecha': colors.red[900],
+        'Music': colors.amber[900],
+        'Mystery': colors.deepPurple[900],
+        'Psychological': colors.lightGreen[900],
+        'Romance': colors.teal[900],
+        'Sci-Fi': colors.indigo[900],
+        'Slice of Life': colors.blue[900],
+        'Sports': colors.yellow[900],
+        'Supernatural': colors.orange[900],
+        'Thriller': colors.cyan[900],
       },
     },
   })
