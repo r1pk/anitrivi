ef99a099c162171d9adc04544b8e215b2b13ae7d
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-06-12 17:57:10 +0200

    Move `query-client.js` to new `lib` folder
    - Move `query-client.js` to new `lib` folder which is a better and more
      accurate place for it because `query-client.js` is not exactly
      a config file but more like a library file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/order */
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@/configs/query-client';
+import { queryClient } from '@/lib/query-client';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { dark } from '@/themes/dark';
